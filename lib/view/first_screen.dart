9fd372ceff430a212e8b63c8649e95dbab4e11c0
Raj Virani <6587rajvirani@gmail.com>
2022-11-06 14:05:25 +0530

    06/11/2022

--- lib/view/first_screen.dart
+++ lib/view/first_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:galaxy_planets/controller/controller_planets.dart';
 import 'package:galaxy_planets/view/secode_screen.dart';
 import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
 
 class Galaxy_Planets_Screen extends StatefulWidget {
   const Galaxy_Planets_Screen({Key? key}) : super(key: key);
@@ -25,88 +26,105 @@ class _Galaxy_Planets_ScreenState extends State<Galaxy_Planets_Screen> with Sing
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.indigo.shade900,
-      appBar: AppBar(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            bottom: Radius.circular(15),
-          ),
-        ),
-        leading: Icon(Icons.dehaze_outlined),
-        title: Text("GALAXY PLANETS"),
-        actions: [
-          Container(padding:EdgeInsets.only(right: 20),child: Icon(Icons.search))
-        ],
-      ),
-      body: ListView.builder(
-        itemCount: galaxy_controller.l1.length,
-        itemBuilder: (context,index){
-          return  InkWell(onTap: (){
-             galaxy_controller.DataPick = galaxy_controller.l1.value[index];
-             Get.to(PlanetsDetlis_Screen());
-          },
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Stack(
-                  alignment: Alignment.centerLeft,
-                  children: [
-                    Container(
-                      child: Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Padding(
-                                padding: EdgeInsets.only(top: 15,right: 40),
-                                child: Text("${galaxy_controller.l1.value[index].Name}",style: TextStyle(color: Colors.white,fontSize: 25),),
-                              ),
-                            ],
-                          ),
 
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Padding(
-                                padding: EdgeInsets.only(right: 30),
-                                child: Text("Milkyway Galaxy",style: TextStyle(color: Colors.grey),),
-                              )
-                            ],
-                          ),
-                          SizedBox(height: 20,),
-
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text("☀️ ${galaxy_controller.l1.value[index].Location}",style: TextStyle(color: Colors.grey)),
-                              SizedBox(width: 15,),
-                              Text("🔁 ${galaxy_controller.l1.value[index].MS}",style: TextStyle(color: Colors.grey)),
-                            ],
-                          ),
-                        ],
-                      ),
-                      margin: EdgeInsets.all(30),
-                      height: 150,
-                      width: MediaQuery.of(context).size.width*0.8,
-                      decoration: BoxDecoration(
-                        color: Colors.indigo.shade600,
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                    ),
-                    AnimatedBuilder(
-                            animation: _animationController!,
-                            builder: (context,v1){
-                              return Transform.rotate(
-                                angle: _animationController!.value*6,
-                                child:v1,
-                              );
-                            },
-                            child: Image.asset("${galaxy_controller.l1.value[index].Image}",width: 80,height: 80,fit: BoxFit.fill,)),
-                  ],
-                ),
-              ],
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300,
+            centerTitle: true,
+            floating: true,
+            pinned: true,
+            backgroundColor: Colors.blue,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Lottie.asset("assets/image/107763-solar-system-kasanima.json",fit: BoxFit.fill),
             ),
-          );
-        },
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+                bottom: Radius.circular(15),
+              ),
+            ),
+            leading: Icon(Icons.dehaze_outlined),
+            title: Text("GALAXY PLANETS",style: TextStyle(color: Colors.white,fontSize: 25),),
+            actions: [
+              Container(padding:EdgeInsets.only(right: 20),child: Icon(Icons.search))
+            ],
+          ),
+           SliverList(
+             delegate: SliverChildBuilderDelegate(
+               childCount: galaxy_controller.l1.length,
+                (context,index){
+                 return  InkWell(onTap: (){
+                   galaxy_controller.DataPick = galaxy_controller.l1.value[index];
+                   Get.to(PlanetsDetlis_Screen());
+                 },
+                   child: Row(
+                     mainAxisAlignment: MainAxisAlignment.end,
+                     children: [
+                       Stack(
+                         alignment: Alignment.centerLeft,
+                         children: [
+                           Container(
+                             child: Column(
+                               children: [
+                                 Row(
+                                   mainAxisAlignment: MainAxisAlignment.center,
+                                   children: [
+                                     Padding(
+                                       padding: EdgeInsets.only(top: 15,right: 40),
+                                       child: Text("${galaxy_controller.l1.value[index].Name}",style: TextStyle(color: Colors.white,fontSize: 25),),
+                                     ),
+                                   ],
+                                 ),
+
+                                 Row(
+                                   mainAxisAlignment: MainAxisAlignment.center,
+                                   children: [
+                                     Padding(
+                                       padding: EdgeInsets.only(right: 30),
+                                       child: Text("Milkyway Galaxy",style: TextStyle(color: Colors.grey),),
+                                     )
+                                   ],
+                                 ),
+                                 SizedBox(height: 20,),
+
+                                 Row(
+                                   mainAxisAlignment: MainAxisAlignment.center,
+                                   children: [
+                                     Text("☀️ ${galaxy_controller.l1.value[index].Location}",style: TextStyle(color: Colors.grey)),
+                                     SizedBox(width: 15,),
+                                     Text("🔁 ${galaxy_controller.l1.value[index].MS}",style: TextStyle(color: Colors.grey)),
+                                   ],
+                                 ),
+                               ],
+                             ),
+                             margin: EdgeInsets.all(30),
+                             height: 150,
+                             width: MediaQuery.of(context).size.width*0.8,
+                             decoration: BoxDecoration(
+                               color: Colors.indigo.shade600,
+                               borderRadius: BorderRadius.circular(8),
+                             ),
+                           ),
+                           AnimatedBuilder(
+                               animation: _animationController!,
+                               builder: (context,v1){
+                                 return Transform.rotate(
+                                   angle: _animationController!.value*6,
+                                   child:v1,
+                                 );
+                               },
+                               child: Image.asset("${galaxy_controller.l1.value[index].Image}",width: 80,height: 80,fit: BoxFit.fill,)),
+                         ],
+                       ),
+                     ],
+                   ),
+                 );
+               },
+             ),
+
+
+          ),
+        ],
 
       ),
     );
--- lib/view/secode_screen.dart
+++ lib/view/secode_screen.dart
@@ -24,161 +24,163 @@ class _PlanetsDetlis_ScreenState extends State<PlanetsDetlis_Screen> with  Singl
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        alignment: Alignment.bottomCenter,
-        children: [
-         Container(
-           child: SingleChildScrollView(
-             child: Stack(
-               alignment: Alignment.center,
-               children: [
-               Column(
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+           Container(
+             child: SingleChildScrollView(
+               child: Stack(
+                 alignment: Alignment.center,
                  children: [
-                   Stack(
-                     alignment: Alignment.topCenter,
-                     children:[
-                       Image.asset("${galaxy_controller.DataPick!.Image2}",height: 420,width: double.infinity,fit: BoxFit.fill,),
-                       Row(
-                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                         children: [
-                         Padding(
-                           padding: const EdgeInsets.all(8.0),
-                           child: IconButton(onPressed: (){
-                             Get.back();
-                           },
-                             icon: Icon(Icons.arrow_back,color: Colors.white,),
-                           ),
-                         ),
+                 Column(
+                   children: [
+                     Stack(
+                       alignment: Alignment.topCenter,
+                       children:[
+                         Image.asset("${galaxy_controller.DataPick!.Image2}",height: 420,width: double.infinity,fit: BoxFit.fill,),
+                         Row(
+                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                           children: [
                            Padding(
                              padding: const EdgeInsets.all(8.0),
-                             child: Icon(Icons.more_vert,color: Colors.white),
-                           )
-                       ],)
-
-                     ],),
-                   SingleChildScrollView(
-                     child: Container(
-                       height: MediaQuery.of(context).size.height*0.5,
-                       color: Colors.indigo.shade900,
-                       child: Center(
-                         child: Column(
-                           children: [
-                             Padding(
-                               padding:  EdgeInsets.only(left: 20),
-                               child: Row(
-                                 mainAxisAlignment: MainAxisAlignment.start,
-                                 children: [
-                                   Column(
-                                     children: [
-                                       SizedBox(height: 150,),
-                                       Text("OVERVIEW",style: TextStyle(color: Colors.white,fontSize: 25),),
-                                       Container(margin: EdgeInsets.only(right: 95),height: 3,width: 40,color: Colors.blue,),
-                                     ],
-                                   ),
-                                 ],
-                               ),
+                             child: IconButton(onPressed: (){
+                               Get.back();
+                             },
+                               icon: Icon(Icons.arrow_back,color: Colors.white,),
                              ),
-                             SizedBox(height: 10,),
-                             Row(
-                               mainAxisAlignment: MainAxisAlignment.center,
-                               children: [
-                                 Container(width: MediaQuery.of(context).size.width*.9,
-                                     child: Text("${galaxy_controller.DataPick!.Overview}",style: TextStyle(color: Colors.white70),))
-                               ],
+                           ),
+                             Padding(
+                               padding: const EdgeInsets.all(8.0),
+                               child: Icon(Icons.more_vert,color: Colors.white),
                              )
-                           ],
+                         ],)
+
+                       ],),
+                     SingleChildScrollView(
+                       child: Container(
+                         height: MediaQuery.of(context).size.height*0.5,
+                         color: Colors.indigo.shade900,
+                         child: Center(
+                           child: Column(
+                             children: [
+                               Padding(
+                                 padding:  EdgeInsets.only(left: 20),
+                                 child: Row(
+                                   mainAxisAlignment: MainAxisAlignment.start,
+                                   children: [
+                                     Column(
+                                       children: [
+                                         SizedBox(height: 150,),
+                                         Text("OVERVIEW",style: TextStyle(color: Colors.white,fontSize: 25),),
+                                         Container(margin: EdgeInsets.only(right: 95),height: 3,width: 40,color: Colors.blue,),
+                                       ],
+                                     ),
+                                   ],
+                                 ),
+                               ),
+                               SizedBox(height: 10,),
+                               Row(
+                                 mainAxisAlignment: MainAxisAlignment.center,
+                                 children: [
+                                   Container(width: MediaQuery.of(context).size.width*.9,
+                                       child: Text("${galaxy_controller.DataPick!.Overview}",style: TextStyle(color: Colors.white70),))
+                                 ],
+                               )
+                             ],
+                           ),
                          ),
                        ),
                      ),
-                   ),
-                 ],
-               ),
-                 Stack(
-                   alignment: Alignment.topCenter,
-                   children: [
-                     Container(
-                       margin: EdgeInsets.only(top: 50,left: 10,right: 10,bottom: 10),
-                       height: 200,
-                       width: MediaQuery.of(context).size.width*.9,
-                       decoration: BoxDecoration(
-                         color: Colors.indigo.shade600,
-                         borderRadius: BorderRadius.circular(10),),
-                         child: Row(
-                           mainAxisAlignment: MainAxisAlignment.center,
-                           children: [
-                             Column(
-                               children: [
-                                 SizedBox(height: 70,),
-                                 Text("${galaxy_controller.DataPick!.Name}",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 25),),
-                                 Text("Milkyway Galaxy",style: TextStyle(color: Colors.white70),),
-                                 SizedBox(height: 10,),
-                                 Container(
-                                   height: 3,
-                                   width: 50,
-                                   decoration: BoxDecoration(
-                                     color: Colors.blue,
-                                     borderRadius: BorderRadius.circular(10),
-                                   ),
-                                 ),
-                                 SizedBox(height:20,),
-                                 Row(
-                                   mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                   children: [
-                                     Text("☀  ${galaxy_controller.DataPick!.Location}",style: TextStyle(color: Colors.grey)),
-                                     SizedBox(width: 25,),
-                                     Text("🔁  ${galaxy_controller.DataPick!.MS}",style: TextStyle(color: Colors.grey)),
-                                   ],
-                                 ),
-                               ],
-                             )
-                           ],
-                         ),
-                     ),
-                      AnimatedBuilder(
-                          animation: _animationController!,
-                          builder: (context,v2){
-                            return Transform.rotate(
-                              angle: _animationController!.value*6,
-                              child:v2,
-                            );
-                          },child: Image.asset("${galaxy_controller.DataPick!.Image}",height:100,width: 100,fit: BoxFit.fill,)),
                    ],
                  ),
+                   Stack(
+                     alignment: Alignment.topCenter,
+                     children: [
+                       Container(
+                         margin: EdgeInsets.only(top: 50,left: 10,right: 10,bottom: 10),
+                         height: 200,
+                         width: MediaQuery.of(context).size.width*.9,
+                         decoration: BoxDecoration(
+                           color: Colors.indigo.shade600,
+                           borderRadius: BorderRadius.circular(10),),
+                           child: Row(
+                             mainAxisAlignment: MainAxisAlignment.center,
+                             children: [
+                               Column(
+                                 children: [
+                                   SizedBox(height: 70,),
+                                   Text("${galaxy_controller.DataPick!.Name}",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 25),),
+                                   Text("Milkyway Galaxy",style: TextStyle(color: Colors.white70),),
+                                   SizedBox(height: 10,),
+                                   Container(
+                                     height: 3,
+                                     width: 50,
+                                     decoration: BoxDecoration(
+                                       color: Colors.blue,
+                                       borderRadius: BorderRadius.circular(10),
+                                     ),
+                                   ),
+                                   SizedBox(height:20,),
+                                   Row(
+                                     mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                     children: [
+                                       Text("☀  ${galaxy_controller.DataPick!.Location}",style: TextStyle(color: Colors.grey)),
+                                       SizedBox(width: 25,),
+                                       Text("🔁  ${galaxy_controller.DataPick!.MS}",style: TextStyle(color: Colors.grey)),
+                                     ],
+                                   ),
+                                 ],
+                               )
+                             ],
+                           ),
+                       ),
+                        AnimatedBuilder(
+                            animation: _animationController!,
+                            builder: (context,v2){
+                              return Transform.rotate(
+                                angle: _animationController!.value*6,
+                                child:v2,
+                              );
+                            },child: Image.asset("${galaxy_controller.DataPick!.Image}",height:100,width: 100,fit: BoxFit.fill,)),
+                     ],
+                   ),
 
-              ],
-             ),
-           ),
-        ),
-          InkWell(
-            onTap: (){
-              return BottomBarDilaog();
-            },
-            child: Container(height: 70,
-              color: Colors.blue,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(top: 6,bottom: 6,left: 15),
-                    child: Column(
-                      children: [
-                        Text("START FROM",style: TextStyle(color: Colors.white70),),
-                        Text("\$${galaxy_controller.DataPick!.Price}m",style: TextStyle(fontSize: 25,color: Colors.white),),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding:  EdgeInsets.only(top: 6,bottom: 6,right: 20),
-                    child: Text("BOOK NOW >",style: TextStyle(fontSize: 25,color: Colors.white),),
-                  )
                 ],
+               ),
+             ),
+          ),
+            InkWell(
+              onTap: (){
+                return BottomBarDilaog();
+              },
+              child: Container(height: 70,
+                color: Colors.blue,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 6,bottom: 6,left: 15),
+                      child: Column(
+                        children: [
+                          Text("START FROM",style: TextStyle(color: Colors.white70),),
+                          Text("\$${galaxy_controller.DataPick!.Price}m",style: TextStyle(fontSize: 25,color: Colors.white),),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding:  EdgeInsets.only(top: 6,bottom: 6,right: 20),
+                      child: Text("BOOK NOW >",style: TextStyle(fontSize: 25,color: Colors.white),),
+                    )
+                  ],
+                ),
               ),
-            ),
-          )
-       ],
+            )
+         ],
 
-      )
+        )
+      ),
     );
   }
 
